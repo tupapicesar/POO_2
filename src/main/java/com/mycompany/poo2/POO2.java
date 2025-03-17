ef500756c98dea929ba0f37445dd04e36400c719
tupapicesar <cesarsteven305@gmail.com>
2025-03-16 19:39:27 -0500

    nuevi proyecto

---
+++ src/main/java/com/mycompany/poo2/POO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo2;
+
+/**
+ *
+ * @author hilla
+ */
+public class POO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
